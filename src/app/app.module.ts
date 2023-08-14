6e251ac233651f8ca9829182a2ca585f0efd96a4
Anul Jain <anuljain23@gmail.com>
2023-08-14 18:37:05 +0530

    Initial Commit - E-commerce web app using angular. Created UI for seller signup page. Created json server for fake APIs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { HomeComponent } from './MyComponents/home/home.component';
+import { SellerAuthComponent } from './MyComponents/seller-auth/seller-auth.component';
+import { FormsModule } from '@angular/forms';
+import { WebsiteComponent } from './MyComponents/website/website.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SellerAuthComponent,
+    WebsiteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
